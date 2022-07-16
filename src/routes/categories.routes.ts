863a7307f6bc742dbc23cb0ae9391ad3aa9021ff
Raphael Lima <phaelslima@gmail.com>
2022-07-16 16:35:11 -0300

    feat: add list categories useCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,15 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get('/', (resquest, response) => {
-  const { name, description } = resquest.body;
-
-  const categories = categoriesRepository.list();
-
-  return response.status(200).json(categories);
+categoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
